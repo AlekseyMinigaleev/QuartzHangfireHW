30b618b2ba0a8d1d4a93447e5d2c699956e445b9
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-07-24 11:49:30 +0500

    Add shared project

---
+++ Shared/StorageCreator.cs
@@ -0,0 +1,34 @@
+﻿using Npgsql;
+
+namespace Shared
+{
+    public static class StorageCreator
+    {
+        public static void CreateDatabase(string connectionString)
+        {
+            var connectionStingBuilder =
+                new NpgsqlConnectionStringBuilder(connectionString);
+            var targetDatabase = connectionStingBuilder.Database!;
+
+            ExecuteSQL(connectionStingBuilder, targetDatabase);
+        }
+
+        private static void ExecuteSQL(
+            NpgsqlConnectionStringBuilder connectionStingBuilder,
+            string targetDatabase)
+        {
+            connectionStingBuilder.Database = "postgres";
+
+            using var connection = new NpgsqlConnection(connectionStingBuilder.ConnectionString);
+            connection.Open();
+            using var cmd = new NpgsqlCommand($"SELECT 1 FROM pg_database WHERE datname = '{targetDatabase}'", connection);
+            var exists = cmd.ExecuteScalar() != null;
+
+            if (!exists)
+            {
+                using var createCmd = new NpgsqlCommand($"CREATE DATABASE \"{targetDatabase}\"", connection);
+                createCmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
